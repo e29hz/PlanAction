4187e36d9520a0daad1ec055edb4bde555e43f57
ehongzhen <ehongzhen@qq.com>
2016-03-29 16:31:28 +0800

    Initial Commit

---
+++ PlanAction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlanAction
+//
+//  Created by 鄂鸿桢 on 16/3/29.
+//  Copyright © 2016年 e29hz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
